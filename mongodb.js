477aa02b8b7de49df869f1f311ce75facff76439
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-12 19:52:40 +0530

    MongoDB setup and a document added

--- mongodb.js
+++ mongodb.js
@@ -1,1 +1,28 @@
+'use strict'
 // CRUD create read update and delete
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient
+ 
+const connectionURL = 'mongodb://127.0.0.1/27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database');
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'Sankalp',
+        age: 21
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user.');
+        }
+
+        console.log(result.ops);
+    });
+
+});

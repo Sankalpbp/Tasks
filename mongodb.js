7ede9ecabfc0cc00be528d806406757c9482da76
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-19 08:34:32 +0530

    Reading some more data

--- mongodb.js
+++ mongodb.js
@@ -3,9 +3,6 @@
 
 const { MongoClient, ObjectID } = require('mongodb');
 
-const id = new ObjectID();
-console.log(id);
-console.log(id.getTimestamp());
  
 const connectionURL = 'mongodb://127.0.0.1/27017';
 const databaseName = 'task-manager';
@@ -17,19 +14,15 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName);
 
-    db.collection('users').findOne({ _id: new ObjectID("5f0b1ce27d6617758e41cb81") }, (error, user) => {
+    db.collection('users').findOne({ _id: new ObjectID("5f131257eab371273bde68c4") }, (errro, user) => {
         if (error) {
-            return console.log('Unable to fetch user.');
+            return console.log('Unable to fetch the user');
         } 
 
         console.log(user);
     });
 
-    db.collection('users').find({ age: 22 }).toArray((error, users) => {
-        console.log(users);
-    });
-
-    db.collection('users').find({ age: 22 }).count((error, count) => {
-        console.log(count);
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks);
     });
 });

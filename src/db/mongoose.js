842ddb81d6718e82c133e427be153e0592972958
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-21 21:00:41 +0530

    Creating REST API route for storing tasks on the database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -13,16 +13,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     console.log('connection successful');
 });
 
-// creating the task model
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    }, 
-    completed: {
-        type: Boolean,
-        required: false,
-        default: false
-    }
-});

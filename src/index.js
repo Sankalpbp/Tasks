4e54ba99865c42911301ab60391950e174108ee1
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-23 19:56:08 +0530

    Setting Update and Delete Routes

--- src/index.js
+++ src/index.js
@@ -47,6 +47,46 @@ app.get('/users/:id', async (req, res) => {
     }
 });
 
+app.patch('/users/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+
+    if (isValidOperation === false) {
+        return res.status(400).send({ error: 'Invalid updates!'});
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (user === null) {
+            return res.status(404).send();
+        } 
+
+        res.send(user); 
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        
+        if (user === null) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}); 
+
 app.post('/tasks', async (req, res) => {
 
     const task = new Task(req.body);
@@ -79,6 +119,46 @@ app.get('/tasks/:id', async (req, res) => {
         if (task === null) {
             return res.status(404).send();
         }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+
+    if (isValidOperation === false) {
+        return res.status(400).send({error: 'Invalid updates!'});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (task === null) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if (task === null) {
+            return res.status(404).send();
+        }
+
         res.send(task);
     } catch (e) {
         res.status(500).send(e);

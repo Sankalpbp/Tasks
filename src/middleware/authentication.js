bd3f19fa0403b4ea24ff70699f3f40890a99e529
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-24 21:04:59 +0530

    Deleting /users route and Creating /users/me route

---
+++ src/middleware/authentication.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.js');
+
+const authentication = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'wearedevelopers');
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        if (user === null) {
+            throw new Error();
+        }
+
+        req.user = user;
+        next();
+    } catch (e) {
+        res.status(401).send({ error: 'Please Authenticate' });
+    }
+};
+
+module.exports = authentication;

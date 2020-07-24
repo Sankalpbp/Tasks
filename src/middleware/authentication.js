226d6682a94a81cdb8434f95f3754f1ccf8ee5d7
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-24 21:23:59 +0530

    Logging out route handler

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -13,6 +13,7 @@ const authentication = async (req, res, next) => {
             throw new Error();
         }
 
+        req.token = token;
         req.user = user;
         next();
     } catch (e) {
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,6 +4,7 @@ const express = require('express');
 const User = require('../models/user.js');
 const router = new express.Router();
 const authentication = require('../middleware/authentication.js');
+const { ReplSet } = require('mongodb');
 
 router.post('/users', async (req, res) => {
 
@@ -26,6 +27,32 @@ router.post('/users/login', async (req, res) => {
         res.send({ user, token });
     } catch (e) {
         res.status(400).send(e);
+    }
+});
+
+router.post('/users/logout', authentication, async (req, res) => {
+
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+
+        await req.user.save();
+
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.post('/users/logoutAll', authentication, async (req, res) => {
+    
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send();
     }
 });
 

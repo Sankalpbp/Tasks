98cef0e6cadc0411607a50f1020e0a1627ee4459
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-25 14:02:26 +0530

    Finalizing Authentication

--- src/routers/user.js
+++ src/routers/user.js
@@ -60,22 +60,7 @@ router.get('/users/me', authentication, async (req, res) => {
     res.send(req.user);
 });
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findById(_id);
-        if (user === null) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', authentication, async (req, res) => {
 
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
@@ -88,8 +73,7 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-
-        const user = await User.findById(req.params.id);
+        const user = req.user;
 
         updates.forEach((update) => {
             user[update] = req.body[update];
@@ -97,26 +81,17 @@ router.patch('/users/:id', async (req, res) => {
 
         await user.save();
 
-        if (user === null) {
-            return res.status(404).send();
-        } 
-
         res.send(user); 
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', authentication, async (req, res) => {
 
     try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        
-        if (user === null) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
+        await req.user.remove();
+        res.send(req.user);
     } catch (e) {
         res.status(500).send(e);
     }

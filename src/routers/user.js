708febc366ca214192f8c64b30dee06654a2c1be
Sankalp Arora <sankalpnitj@gmail.com>
2020-07-26 16:13:02 +0530

    Validating, handling and Serving up files

--- src/routers/user.js
+++ src/routers/user.js
@@ -6,6 +6,7 @@ const router = new express.Router();
 const authentication = require('../middleware/authentication.js');
 const { ReplSet } = require('mongodb');
 const multer = require('multer');
+const sharp = require('sharp');
 
 router.post('/users', async (req, res) => {
 
@@ -99,11 +100,47 @@ router.delete('/users/me', authentication, async (req, res) => {
 }); 
 
 const upload = multer({
-    dest: 'avatars'
+    limits: {
+        fileSize: 200000
+    }, 
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an Image.'));
+        }
+
+        cb(undefined, true);
+    }
 });
 
-router.post('/users/me/avatar', upload.single('avatar'),(req, res) => {
+router.post('/users/me/avatar', authentication, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
     res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+});
+
+router.delete('/users/me/avatar', authentication, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save();
+    res.send();
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(404).send();
+    }
 });
 
 module.exports = router;
